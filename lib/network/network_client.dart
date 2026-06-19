7601616a95345bb83447f8732635302b59c4b501
AbuRasel51 <rasel@primetechbd.com>
2026-06-20 01:48:49 +0600

    font add network client add

---
+++ lib/network/network_client.dart
@@ -0,0 +1,20 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+class NetworkClient{
+  Future<dynamic>get(String url)async{
+    try{
+      final response=await http.get(Uri.parse(url));
+      if(response.statusCode==200){
+        return json.decode(response.body);
+      }else{
+        throw Exception("Failed to load data : ${response.statusCode}");
+      }
+
+    }catch (e){
+      throw Exception("Error $e");
+    }
+
+  }
+}
