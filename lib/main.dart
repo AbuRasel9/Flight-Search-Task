99d395648b3066a80b1024ebd8f104a3e620872c
AbuRasel51 <rasel@primetechbd.com>
2026-06-20 01:49:10 +0600

    font add network client add

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flight_search/configs/enums/font_options.dart';
+import 'package:flight_search/configs/theme/app_theme_data.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flight Search',
+      debugShowCheckedModeBanner: false,
+      theme: AppThemeData.lightThemeData(FontOptions.montserrat),
+      darkTheme: AppThemeData.darkThemeData(FontOptions.montserrat),
+      themeMode: ThemeMode.light,
+      home: c
+    );
+  }
+}
